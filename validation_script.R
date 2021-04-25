ae4123d5de3d19843fcb1e49743cb6026ad57b66
prkassel <prkassel@gmail.com>
2021-04-24 21:16:18 -0500

    clean up on validation script. Validation test scored 0.857

--- validation_script.R
+++ validation_script.R
@@ -11,6 +11,7 @@ if(!require(data.table)) install.packages("data.table", repos = "http://cran.us.
 library(tidyverse)
 library(caret)
 library(data.table)
+library(lubridate)
 
 # MovieLens 10M dataset:
 # https://grouplens.org/datasets/movielens/10m/
@@ -55,43 +56,74 @@ rm(dl, ratings, movies, test_index, temp, movielens, removed)
 # End Create edx set
 ###############################################
 
+edx$release_year <- str_sub(edx$title,start= -6)
+edx$release_year <- as.numeric(str_extract(edx$release_year, "\\d+"))
+edx$rating_year <- year(as.Date(as.POSIXct(edx$timestamp,origin="1970-01-01")))
+# Round to the nearest 10 year increment
+edx$years_from_release <- round((edx$rating_year - edx$release_year) / 10) * 10
+
+validation$release_year <- str_sub(validation$title,start= -6)
+validation$release_year <- as.numeric(str_extract(validation$release_year, "\\d+"))
+validation$rating_year <- year(as.Date(as.POSIXct(validation$timestamp,origin="1970-01-01")))
+# Round to the nearest 10 year increment
+validation$years_from_release <- round((validation$rating_year - validation$release_year) / 10) * 10
+
+RMSE <- function(true_ratings, predicted_ratings){
+  sqrt(mean((true_ratings - predicted_ratings)^2))
+}
+
 ###############################################
 # TRAIN MODEL
 ###############################################
 
 mu <- mean(edx$rating)
 
-movies_df <- edx %>% group_by(movieId) %>% summarise(movie_bias=sum(rating - mu)/(n() + 2 )) %>% select(movieId, movie_bias)
-users_df <- edx %>% group_by(userId) %>% summarise(user_bias=sum(rating - mu)/(n() + 6)) %>% select(userId, user_bias)
+#### Calculating movie effect
+movies_df <- edx %>% group_by(movieId) %>% summarise(movie_bias=sum(rating - mu)/(n() + 2.5 )) %>% select(movieId, movie_bias)
+edx <- edx %>% inner_join(movies_df, on="movieId")
 
 
+### Calculating User Effect
+users_df <- edx %>% group_by(userId) %>% summarise(user_bias=sum(rating - movie_bias - mu)/(n())) %>% select(userId, user_bias)
+edx <- edx %>% inner_join(users_df, on="userId")
+
+### Calculate User Genre Effect
 genre_ratings_df <- edx %>% separate_rows(genres, sep="\\|")
+user_genres_df <- genre_ratings_df %>% group_by(userId, genres) %>% 
+  summarise(genre_bias = sum(rating - movie_bias - user_bias - mu)/(n() + 10)) %>% 
+  inner_join(genre_ratings_df, on=genres) %>% select(userId, genres, genre_bias, movieId)
 
-user_genre_biases <- genre_ratings_df %>% group_by(userId, genres) %>% summarise(user_genre_bias = sum(rating - mu)/(n() + 3.5))
+user_genres_df_wide <- user_genres_df %>% pivot_wider(names_from = genres, values_from=genre_bias)
+edx <- edx %>% inner_join(user_genres_df_wide, on=c("userId", "movieId"))
+edx$user_genre_bias <- apply(X=edx[,12:ncol(edx)], MARGIN=1, FUN=mean, na.rm=TRUE)
 
-user_genre_ratings <- genre_ratings_df %>% inner_join(user_genre_biases, on=c("userId", "genres"))
-user_genre_ratings <- user_genre_ratings %>% select(-rating,-timestamp,-title) %>% pivot_wider(names_from=genres, values_from="user_genre_bias")
+
+edx <- edx %>% select(-title, -genres, -timestamp, -release_year, -rating_year)
+edx$user_genre_bias <- apply(X=edx[,6:ncol(edx)], MARGIN=1, FUN=mean, na.rm=TRUE)
+
+#### Time Effect
+recency_df <- edx %>% group_by(years_from_release) %>% 
+  summarise(recency_bias=sum(rating - movie_bias - user_bias - user_genre_bias - mu)/(n() + 10)) %>% select(years_from_release, recency_bias)
 
 #############
 # Validation Test
 ############
+validation_genre_ratings_df <- validation %>% separate_rows(genres, sep="\\|")
 
-validation <- validation %>% select(-timestamp, -title)
-validation_ratings_by_genre <- validation %>% separate_rows(genres, sep="\\|")
-validation_ratings_by_genre <- validation_ratings_by_genre %>% inner_join(user_genre_biases, on=c("userId", "genres"))
-validation_user_genre_biases <-  validation_ratings_by_genre %>% select(-rating) %>% pivot_wider(names_from=genres, values_from="user_genre_bias")
+validation <- validation %>% left_join(movies_df, on="movieId") %>% 
+  left_join(users_df, on="userId") %>% left_join(recency_df, on="years_from_release")
+validation_user_genres_df <- user_genres_df %>% select(-movieId) %>% group_by(userId, genres) %>% summarise(genre_bias = mean(genre_bias))
+validation_genre_biases <- validation_genre_ratings_df %>% left_join(validation_user_genres_df, on=(c("userId", "genres"))) %>% select(userId, movieId, genres, genre_bias)
+validation_genre_biases <- validation_genre_biases %>% pivot_wider(names_from = genres, values_from=genre_bias)
+validation <-validation %>% left_join(validation_genre_biases, on=c("userId", "movieId"))
 
-validation <- validation %>% inner_join(validation_user_genre_biases, on=c("userId", "movieId"))
-validation <- validation %>% inner_join(movies_df, on="movieId")
-validation <- validation %>% inner_join(users_df, on="movieId")
-validation <- validation %>% select(-genres)
+validation$user_genre_bias <- apply(X=validation[,12:ncol(validation)], MARGIN=1, FUN=mean, na.rm=TRUE)
+validation$user_genre_bias[is.na(validation$user_genre_bias)] = 0
 
-validation$user_genre_bias <- apply(X=validation[,4:(ncol(validation) - 2)], MARGIN=1, FUN=mean, na.rm=TRUE)
-validation$max_bias <- apply(X=validation[,(ncol(validation) - 3): ncol(validation)], MARGIN=1, FUN=max, na.rm=TRUE)
-validation$min_bias <- apply(X=validation[,(ncol(validation) - 4): (ncol(validation) - 1)], MARGIN=1, FUN=min, na.rm=TRUE)
-validation$net_bias <- validation$max_bias + validation$min_bias
+validation <- validation %>% select(userId, movieId, rating, movie_bias, user_bias, user_genre_bias, recency_bias)
 
+validation[is.na(validation)] = 0
 
-validation$pred <- validation$net_bias + mu
+validation$pred<- validation$movie_bias + validation$user_bias + validation$user_genre_bias + validation$recency_bias + mu
 
 print(RMSE(validation$rating, validation$pred))
